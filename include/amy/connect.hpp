0f870127e71b4bd9584e24224a4eba059d1f21c0
Cheng Lian <lian.cs.zju@gmail.com>
2011-04-22 18:47:11 +0800

    Merge param_connect.hpp into connect.hpp.
    - Remove redundant connect() and async_connect() overloads.

--- include/amy/connect.hpp
+++ include/amy/connect.hpp
@@ -6,39 +6,11 @@
 #include <amy/endpoint_traits.hpp>
 
 #include <boost/bind.hpp>
+#include <boost/parameter.hpp>
 #include <boost/system/error_code.hpp>
 
 namespace amy {
-
-template<typename MySQLConnector>
-void connect(MySQLConnector& connector,
-             client_flags flags = default_flags)
-{
-    connector.connect(null_endpoint(), auth_info(), "", flags);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint
->
-void connect(MySQLConnector& connector,
-             Endpoint const& endpoint,
-             client_flags flags = default_flags)
-{
-    connector.connect(endpoint, auth_info(), "", flags);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint
->
-void connect(MySQLConnector& connector,
-             Endpoint const& endpoint,
-             auth_info const& auth,
-             client_flags flags = default_flags)
-{
-    connector.connect(endpoint, auth, "", flags);
-}
+namespace detail {
 
 template<
     typename MySQLConnector,
@@ -59,109 +31,12 @@ template<
 >
 boost::system::error_code connect(MySQLConnector& connector,
                                   Endpoint const& endpoint,
-                                  boost::system::error_code& ec)
-{
-    return connector.connect(endpoint, auth_info(), "", default_flags, ec);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint
->
-boost::system::error_code connect(MySQLConnector& connector,
-                                  Endpoint const& endpoint,
-                                  auth_info const& auth,
-                                  boost::system::error_code& ec)
-{
-    return connector.connect(endpoint, auth, "", default_flags, ec);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint
->
-boost::system::error_code connect(MySQLConnector& connector,
-                                  Endpoint const& endpoint,
-                                  auth_info const& auth,
-                                  std::string const& database,
-                                  boost::system::error_code& ec)
-{
-    return connector.connect(endpoint, auth, database, default_flags, ec);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint
->
-boost::system::error_code connect(MySQLConnector& connector,
-                                  Endpoint const& endpoint,
-                                  auth_info const& auth,
-                                  client_flags flags,
-                                  boost::system::error_code& ec)
-{
-    return connector.connect(endpoint, auth, "", flags, ec);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint
->
-boost::system::error_code connect(MySQLConnector& connector,
-                                  Endpoint const& endpoint,
                                   auth_info const& auth,
                                   std::string const& database,
                                   client_flags flags,
                                   boost::system::error_code& ec)
 {
     return connector.connect(endpoint, auth, database, flags, ec);
-}
-
-template<
-    typename MySQLConnector,
-    typename ConnectHandler
->
-void async_connect(MySQLConnector& connector, ConnectHandler handler) {
-    async_connect(connector, null_endpoint(),
-                  auth_info(), "", default_flags, handler);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint,
-    typename ConnectHandler
->
-void async_connect(MySQLConnector& connector,
-                   Endpoint const& endpoint,
-                   ConnectHandler handler)
-{
-    async_connect(connector, endpoint, auth_info(), "", default_flags, handler);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint,
-    typename ConnectHandler
->
-void async_connect(MySQLConnector& connector,
-                   Endpoint const& endpoint,
-                   auth_info const& auth,
-                   ConnectHandler handler)
-{
-    async_connect(connector, endpoint, auth, "", default_flags, handler);
-}
-
-template<
-    typename MySQLConnector,
-    typename Endpoint,
-    typename ConnectHandler
->
-void async_connect(MySQLConnector& connector,
-                   Endpoint const& endpoint,
-                   auth_info const& auth,
-                   std::string const& database,
-                   ConnectHandler handler)
-{
-    async_connect(connector, endpoint, auth, database, default_flags, handler);
 }
 
 template<
@@ -177,6 +52,58 @@ void async_connect(MySQLConnector& connector,
                    ConnectHandler handler)
 {
     connector.async_connect(endpoint, auth, database, flags, handler);
+}
+
+}   //  namespace detail
+
+namespace keyword {
+
+BOOST_PARAMETER_NAME(connector)
+BOOST_PARAMETER_NAME(endpoint)
+BOOST_PARAMETER_NAME(auth)
+BOOST_PARAMETER_NAME(database)
+BOOST_PARAMETER_NAME(flags)
+BOOST_PARAMETER_NAME(handler)
+
+}   //  namespace keyword
+
+BOOST_PARAMETER_FUNCTION(
+        (void),
+        connect,
+        keyword::tag,
+
+        (required
+            (in_out(connector), *))
+
+        (optional
+            (endpoint, *, amy::null_endpoint())
+            (auth,     *, amy::auth_info())
+            (database, *, std::string())
+            (flags,    *, amy::default_flags)))
+{
+    detail::connect(connector, endpoint, auth, database, flags);
+}
+
+inline void ignore_connect_event(boost::system::error_code const&) {
+}
+
+BOOST_PARAMETER_FUNCTION(
+        (void),
+        async_connect,
+        keyword::tag,
+
+        (required
+            (in_out(connector), *))
+
+        (optional
+            (endpoint, *, amy::null_endpoint())
+            (auth,     *, amy::auth_info())
+            (database, *, std::string())
+            (flags,    *, amy::default_flags)
+            (handler,  *, &ignore_connect_event)))
+{
+    detail::async_connect(
+            connector, endpoint, auth, database, flags, handler);
 }
 
 }   //  namespace amy
