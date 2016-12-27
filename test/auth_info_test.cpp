c6aa5fdcd1cf992c7c3aa5d7c3324e676a6a8044
Cheng Lian <lian.cs.zju@gmail.com>
2016-12-26 23:46:53 -0800

    Make test code compatible with lower versions of Boost.Test

--- test/auth_info_test.cpp
+++ test/auth_info_test.cpp
@@ -2,40 +2,42 @@
 
 #include <amy/auth_info.hpp>
 
+#include <cstring>
+
 BOOST_AUTO_TEST_CASE(should_construct_instance_with_anonymous_user) {
     amy::auth_info auth;
-    BOOST_TEST("" == auth.user());
+    BOOST_CHECK(std::strcmp("", auth.user()) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(should_construct_instance_without_password) {
     amy::auth_info auth;
-    BOOST_TEST(static_cast<const char*>(nullptr) == auth.password());
+    BOOST_CHECK(static_cast<const char*>(nullptr) == auth.password());
 }
 
 BOOST_AUTO_TEST_CASE(should_construct_instance_with_user) {
     amy::auth_info auth("user");
-    BOOST_TEST("user" == auth.user());
+    BOOST_CHECK(std::strcmp("user", auth.user()) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(
     should_construct_instance_with_user_and_nonempty_password
 ) {
     amy::auth_info auth("user", "secret");
-    BOOST_TEST("user", auth.user());
-    BOOST_TEST("secret" == auth.password());
+    BOOST_CHECK(std::strcmp("user", auth.user()) == 0);
+    BOOST_CHECK(std::strcmp("secret", auth.password()) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(should_return_previously_set_password) {
     amy::auth_info auth;
     auth.password("secret");
-    BOOST_TEST("secret" == auth.password());
+    BOOST_CHECK(std::strcmp("secret", auth.password()) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(should_return_null_after_clearing_password) {
     amy::auth_info auth;
     auth.password("secret");
     auth.clear_password();
-    BOOST_TEST(static_cast<const char*>(nullptr) == auth.password());
+    BOOST_CHECK(static_cast<const char*>(nullptr) == auth.password());
 }
 
 // vim:ft=cpp sw=4 ts=4 tw=80 et
