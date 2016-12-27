722616f0ecc938cc7a576eb4b0b74aa49cea4217
Cheng Lian <lian.cs.zju@gmail.com>
2016-12-27 00:18:37 -0800

    Replace boost/stdint.hpp with cstdint

--- include/amy/detail/mysql_types.hpp
+++ include/amy/detail/mysql_types.hpp
@@ -3,7 +3,7 @@
 
 #include <amy/detail/mysql.hpp>
 
-#include <boost/cstdint.hpp>
+#include <cstdint>
 
 namespace amy {
 namespace detail {
@@ -71,36 +71,36 @@ const int shared_memory_base_name = MYSQL_SHARED_MEMORY_BASE_NAME;
 
 #ifdef WIN32
 
-using boost::int8_t;
-using boost::int_least8_t;
-using boost::int_fast8_t;
-using boost::uint8_t;
-using boost::uint_least8_t;
-using boost::uint_fast8_t;
+using std::int8_t;
+using std::int_least8_t;
+using std::int_fast8_t;
+using std::uint8_t;
+using std::uint_least8_t;
+using std::uint_fast8_t;
 
-using boost::int16_t;
-using boost::int_least16_t;
-using boost::int_fast16_t;
-using boost::uint16_t;
-using boost::uint_least16_t;
-using boost::uint_fast16_t;
+using std::int16_t;
+using std::int_least16_t;
+using std::int_fast16_t;
+using std::uint16_t;
+using std::uint_least16_t;
+using std::uint_fast16_t;
 
-using boost::int32_t;
-using boost::int_least32_t;
-using boost::int_fast32_t;
-using boost::uint32_t;
-using boost::uint_least32_t;
-using boost::uint_fast32_t;
+using std::int32_t;
+using std::int_least32_t;
+using std::int_fast32_t;
+using std::uint32_t;
+using std::uint_least32_t;
+using std::uint_fast32_t;
 
-using boost::int64_t;
-using boost::int_least64_t;
-using boost::int_fast64_t;
-using boost::uint64_t;
-using boost::uint_least64_t;
-using boost::uint_fast64_t;
+using std::int64_t;
+using std::int_least64_t;
+using std::int_fast64_t;
+using std::uint64_t;
+using std::uint_least64_t;
+using std::uint_fast64_t;
 
-using boost::intmax_t;
-using boost::uintmax_t;
+using std::intmax_t;
+using std::uintmax_t;
 
 #endif // ifdef WIN32
 
